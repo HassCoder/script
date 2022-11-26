080b23c54d5b0f988c4779f18a743ccbb4985329
HassCoder <109627448+HassCoder@users.noreply.github.com>
2022-11-26 12:56:17 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.askmescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | AskMe - The Ultimate PHP Question & Answers Platform
+// | Copyright (c) 2019 AskMe. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "ans_db";
+
+// Site URL
+$site_url = "https://localhost/script"; // e.g (http://example.com)
+
+$app = "AskMe";
+
+// Purchase code
+$purchase_code = "abuya"; // Your purchase code, don't give it to anyone.
+?>
